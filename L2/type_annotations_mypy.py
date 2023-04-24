6b909ecf79a0d51c3ffc1099560cbaa72d8d8d4f
li <yanfali@xiaoe-tech.com>
2023-04-24 23:16:24 +0800

    更新debug\类型注解\数据类

---
+++ L2/type_annotations_mypy.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/24 21:59
+# @Author  : yanfali
+# @File    : type_annotations_mypy.py
+# @Software: PyCharm
+from typing import List
+
+a:List[int]=[]
+a=[1,2,"aaa"]
